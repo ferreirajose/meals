8d0507efc81e2742e507aeabde8f3ff25c93ef91
José Ferreira da Silva <ferreirajosejp@gmail.com>
2020-07-26 20:48:18 -0300

    add modelo para as categorias add tela de categorias add tela home page add grid view

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:meals/pages/my_home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
