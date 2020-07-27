a79a5c214094bf695f48f7c67d8acf112b9acd83
José Ferreira da Silva <ferreirajosejp@gmail.com>
2020-07-26 21:45:55 -0300

    add fontes Raleway e RobotoCondesend add cor para appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meals/pages/my_home_page.dart';
+import 'package:meals/screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,11 +7,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: CategoriesScreen(),
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'),
+            ),
       ),
-      home: MyHomePage(),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,17 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import 'package:meals/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
+  final bodyPage = GridView(
+    // permiter gerar scroll vertical
+    padding: const EdgeInsets.all(25),
+    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+      maxCrossAxisExtent: 200,
+      childAspectRatio: 3 / 2,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+    ),
+    children: DUMMY_CATEGORIES.map((cat) {
+      // criando uma lista<Widget> do tipo CategoryItem
+      return CategoryItem(cat);
+    }).toList(),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      // permiter gerar scroll vertical
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      children: <Widget>[],
-    );
+    return Platform.isIOS
+        ? CupertinoPageScaffold(child: bodyPage)
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Vamos Cozinhar'),
+            ),
+            body: bodyPage);
   }
 }
