9d1ac983b59c218e18d672b510aedbf7ed32bf41
Jose Ferreira da Silva <ferreirajosejp@gmail.com>
2020-08-05 21:34:34 -0300

    refatorando tela de tabs add estado para controlar os index das tabs utilizando o component BottoNavigationBarItem para criar as tabs add titulo dinamico nas paginas

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,32 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  // @override
+  // Widget build(BuildContext context) {
+  //   return DefaultTabController(
+  //     length: 2, // informando a quantidade de tabs
+  //     child: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text('Vamos cozinhar'),
+  //         bottom: TabBar(tabs: [
+  //           Tab(
+  //             icon: Icon(Icons.category),
+  //             text: 'Categorias',
+  //           ),
+  //           Tab(
+  //             icon: Icon(Icons.star),
+  //             text: 'Favoritos',
+  //           )
+  //         ]),
+  //       ),
+  //       body: TabBarView(
+  //         children: [
+  //           CategoriesScreen(),
+  //           FavoriteScreen(),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screen = [
+    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()}
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // informando a quantidade de tabs
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos cozinhar'),
-          bottom: TabBar(tabs: [
-            Tab(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_screen[_selectedScreenIndex]['title']),
+      ),
+      body: _screen[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedScreenIndex,
+          items: [
+            BottomNavigationBarItem(
               icon: Icon(Icons.category),
-              text: 'Categorias',
+              title: Text('Categorias'),
             ),
-            Tab(
+            BottomNavigationBarItem(
               icon: Icon(Icons.star),
-              text: 'Favoritos',
+              title: Text('Favoritos'),
             )
           ]),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
-        ),
-      ),
     );
   }
 }
